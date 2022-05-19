cb22c5f1585424d285ca76eb218626a6b3792ac1
EvgeniyPirozhok <miloigrovoe@gmail.com>
2022-05-19 22:48:55 +0300

    Task 1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const allCategories = document.querySelectorAll(".item");
+console.log("Number of categories:", allCategories.length);
+
+for (let i = 0; i < allCategories.length; i += 1){
+    const allTitle = allCategories[i].querySelectorAll("h2");
+    console.log("Category: ", allTitle[0].textContent);
+    const allItems = allCategories[i].querySelectorAll("ul li");
+    console.log("Elements:", allItems.length);
+}
